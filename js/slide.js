b7b524d74e1525b562d080cbbd30b00d7747c9e6
cxx0505 <381142773@qq.com>
2024-04-25 20:15:34 +0800

    feat: slide

--- js/slide.js
+++ js/slide.js
@@ -2,21 +2,40 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let slides = document.querySelectorAll('.slides img');
-  
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  
-  slideIndex++;
-  
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  
-  slides[slideIndex - 1].style.display = 'block';
-  
-  setTimeout(showSlides, 5000); // 切换图片的时间间隔，这里是2秒
+    let slides = document.querySelectorAll('.slides img');
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = 'block';
+    
+    setTimeout(showSlides, 5000); // 切换图片的时间间隔，这里是5秒
 }
 
+function plusSlides(n) {
+    showSlide(slideIndex += n);
+}
 
+function showSlide(n) {
+    let slides = document.querySelectorAll('.slides img');
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slides[slideIndex - 1].style.display = 'block';
+}
